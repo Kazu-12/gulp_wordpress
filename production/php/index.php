dd2a0b68d5b434cf7926d28343e02644a7864f2a
Takuya Okamoto <t.okamoto@o-bisu.com>
2021-09-04 13:44:17 +0900

    update-20210904

---
+++ production/php/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <?php wp_head(); ?>
+</head>
+<body>
+  <articl class="documentClass">
+    <h1>sample</h1>
+  </articl>
+  <articl id="documentID">
+    <h1>sample</h1>
+  </articl>
+</body>
+<?php get_footer(); ?>
+</html>
